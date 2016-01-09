afdc1e7b0bdcc052d2dab517f8414f3ad343effc
josh <oyejorge@gmail.com>
2016-01-09 09:40:51 -0700

    correct new_form link

--- SimpleBlog.php
+++ SimpleBlog.php
@@ -57,7 +57,7 @@ class SimpleBlog extends SimpleBlogCommon{
 
 		if( common::LoggedIn() && !file_exists(self::$index_file) ){
 			echo '<p>Congratulations on successfully installing Simple Blog for gpEasy.</p> ';
-			echo '<p>You\'ll probably want to get started by '.common::Link('Special_Blog','creating a blog post','cmd=new_form').'.</p>';
+			echo '<p>You\'ll probably want to get started by '.common::Link('Admin_Blog','creating a blog post','cmd=new_form').'.</p>';
 		}
 
 	}
